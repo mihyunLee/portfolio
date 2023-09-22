bb3f97a278ef17f9722d3b6d64c014c93f712bce
Nor <51310674+mihyunLee@users.noreply.github.com>
2023-09-22 20:41:43 +0900

    💄 DESIGN: 프로젝트 글꼴 설정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,8 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 import { reset } from "styled-reset";
+import "./font.css";
 
-const GlobalStyle = createGlobalStyle`
-	${reset}
+const styled = { createGlobalStyle };
+
+const GlobalStyle = styled.createGlobalStyle`
+  ${reset}
+
+  :root {
+    --white: #fff;
+  }
+
+  body {
+    font-family: "SUIT";
+  }
 `;
 
 export default GlobalStyle;
